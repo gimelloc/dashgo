cc1630fa6b837883c80e7d94acd54c85396ccfb9
Gislaine Mello Casa <gislainemcasa@gmail.com>
2023-03-10 18:43:35 -0300

    Add files via upload

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,28 @@
+import { FormControl, FormLabel, Input as ChackraInput, InputProps as ChackraInputProps } from "@chakra-ui/react";
+
+interface InputProps extends ChackraInputProps {
+    name: string;
+    label?: string;
+}
+
+export function Input({name, label, ...rest}: InputProps){
+    return (
+        <FormControl>
+        { !!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+        
+        <ChackraInput
+        id={name}
+        name={name}
+        type="email"
+        focusBorderColor='pink.500'
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: 'gray.900'
+        }}
+        size="lg"
+        {...rest}
+        />
+        </FormControl>
+    );
+}
